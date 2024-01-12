ce59c383829d047d5bc1bb82cdb120d653f70c14
Razvan Tamsa <razvantamsa295@gmail.com>
2024-01-12 15:39:05 +0200

    connect to cache with retry mechanism

--- backend/db/setup.ts
+++ backend/db/setup.ts
@@ -1,4 +1,39 @@
 import { PrismaClient } from '@prisma/client';
+import Redis from 'ioredis';
+
+const redisConfig = {
+  host: process.env.DB_HOST_REDIS,
+  port: Number(process.env.DB_PORT_REDIS),
+  password: process.env.DB_PASSWORD_REDIS
+};
+
+async function checkCacheConnection() {
+  const maxRetries = 5;
+  let retryAttempts = 0;
+
+  try {
+    const redis = new Redis(redisConfig);
+
+    redis.on('connect', () => {
+      console.log('Connected to the cache');
+      retryAttempts = 0;
+    });
+
+    redis.on('error', async (err) => {
+      retryAttempts += 1;
+
+      console.error('Redis connection error:', err.message);
+
+      if (retryAttempts < maxRetries) {
+        console.log(`Retrying to connect to cache attempt: ${retryAttempts}`);
+      } else if (retryAttempts === maxRetries) {
+        console.error(`Max retries reached: ${maxRetries}`);
+      }
+    });
+  } catch (error) {
+    console.error('Error connecting to the cache:', error);
+  }
+}
 
 async function checkDatabaseConnection() {
   const sqlclient = new PrismaClient();
@@ -16,6 +51,8 @@ async function checkDatabaseConnection() {
   try {
     await checkDatabaseConnection();
     global.sqlconnected = true;
+
+    await checkCacheConnection();
   } catch (error) {
     console.error(error);
     global.sqlconnected = false;
