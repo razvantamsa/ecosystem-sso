f6caf0bf3d1918393f81f1aa81b61e8bf5bda36b
Razvan Tamsa <razvantamsa295@gmail.com>
2024-01-12 18:30:43 +0200

    colorful logs

--- backend/common/logger/index.ts
+++ backend/common/logger/index.ts
@@ -1,14 +1,20 @@
+const resetColor = '\x1b[0m';
+
+const redColor = '\x1b[31m';
+const yellowColor = '\x1b[33m';
+const blueColor = '\x1b[34m';
+
 const Logger = {
   info: (message: string) => {
-    console.log(`[INFO]: ${message}`);
+    console.log(blueColor + `[INFO]: ${message}` + resetColor);
   },
 
   warn: (message: string) => {
-    console.warn(`[WARN]: ${message}`);
+    console.warn(yellowColor + `[WARN]: ${message}` + resetColor);
   },
 
   error: (message: string) => {
-    console.error(`[ERROR]: ${message}`);
+    console.error(redColor + `[ERROR]: ${message}` + resetColor);
   }
 };
 
