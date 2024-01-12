8f3ab630633dac41e1a40cbd8331580e3a6fd052
Razvan Tamsa <razvantamsa295@gmail.com>
2024-01-12 17:39:02 +0200

    add globals for redis

--- backend/lib/global.d.ts
+++ backend/lib/global.d.ts
@@ -2,8 +2,12 @@ declare namespace NodeJS {
   export interface Global {
     sqlclient: any;
     sqlconnected: boolean;
+    cacheclient: any;
+    cacheconnected: boolean;
   }
 }
 
 declare var sqlclient: any;
 declare var sqlconnected: boolean;
+declare var cacheclient: any;
+declare var cacheconnected: boolean;
